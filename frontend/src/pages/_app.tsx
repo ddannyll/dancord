fc648e4c259675ddc6958e0137195f5e12ae6cbd
ddannyll <daniel.nguyen.s173@gmail.com>
2023-05-24 22:41:12 +1000

    remove nextauth references

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,8 @@
-import { SessionProvider } from 'next-auth/react'
 import './globals.css'
 
 import type { AppProps } from 'next/app';
 export default function MyApp({ Component, pageProps: {session, ...pageProps} }: AppProps) {
     return (
-        <SessionProvider session={session}>
-            <Component {...pageProps} />
-        </SessionProvider>
+        <Component {...pageProps} />
     )
 }
