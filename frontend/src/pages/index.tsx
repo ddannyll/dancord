8a08e4edc5c16b52753a0dbfb9dbdd4ed7131c1c
ddannyll <daniel.nguyen.s173@gmail.com>
2023-05-24 22:40:22 +1000

    uninstall nextauth

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,16 +1,10 @@
-import { useSession } from 'next-auth/react';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 export default function Index() {
-    const { status } = useSession()
     const router = useRouter()
-    if (status === 'unauthenticated') {
-        router.push('/login')
-    }
-
-    if (status === 'authenticated') {
-        return <div>Authenticated</div>
-    } else {
-        return <div>Loading</div>
-    }
+    return <div>
+        <Link href={'/login'}>login</Link>
+        <Link href={'/register'}>register</Link>
+    </div>
 }
