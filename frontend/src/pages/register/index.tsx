be85a4eeae6a5caaee1b2c906a4b39a252ddd6f2
ddannyll <daniel.nguyen.s173@gmail.com>
2023-05-24 19:50:41 +1000

    confirm password error handled

--- frontend/src/pages/register/index.tsx
+++ frontend/src/pages/register/index.tsx
@@ -1,13 +1,19 @@
+import { log } from 'console'
 import Link from 'next/link'
 import { SubmitHandler, useForm } from 'react-hook-form'
 
 type LoginInputs = {
     email: string
     password: string
+    confirmPassword: string
 }
 
 export default function Register() {
-    const { register, handleSubmit, formState: {errors} } = useForm<LoginInputs>()
+    const {
+        register,
+        handleSubmit,
+        formState: {errors},
+    } = useForm<LoginInputs>({reValidateMode: 'onChange'})
     const onSubmit: SubmitHandler<LoginInputs> = data => {
         console.log(data)
     }
@@ -26,25 +32,36 @@ export default function Register() {
                     <RequiredStar />
                     {errors.email && <span className='text-red-500 font-normal grow text-end'>Email is required</span>}
                 </label>
-                <input id="email" type="text" className={inputClasses} {...register('email', {required: true})}/>
+                <input id="email" type="text" className={inputClasses}
+                    {...register('email', {required: true})}
+                />
 
                 <label htmlFor="password" className={labelClasses}>
                     PASSWORD
                     <RequiredStar />
                     {errors.password && <span className='text-red-500 font-normal grow text-end'>Password is required</span>}
                 </label>
-                <input id="password" type="text" className={inputClasses} {...register('password', {required: true})}/>
+                <input id="password" type="text" className={inputClasses}
+                    {...register('password', {required: true})}
+                />
 
-                <label htmlFor="password" className={labelClasses}>
+                <label htmlFor="confirmPassword" className={labelClasses}>
                     CONFIRM PASSWORD
                     <RequiredStar />
-                    {errors.password && <span className='text-red-500 font-normal grow text-end'>Password is required</span>}
+                    {errors.confirmPassword && <span className='text-red-500 font-normal grow text-end'>Passwords must match</span>}
                 </label>
+                <input id="confirmPassword" type="text" className={inputClasses}
+                    {...register('confirmPassword', {
+                        validate: (value, formValues) => value === formValues.password,
+                    })}
+                />
 
-                <input id="password" type="text" className={inputClasses} {...register('password', {required: true})}/>
-
-                <button className="bg-violet-500 text-violet-50 p-2 rounded mt-5 hover:bg-violet-600 active:bg-violet-700 transition">Register</button>
-                <p className="text-sm">Have an account? <Link href={'/register'} className="text-violet-400 hover:underline">Login</Link></p>
+                <button
+                    className="bg-violet-500 text-violet-50 p-2 rounded mt-5 hover:bg-violet-600 active:bg-violet-700 transition"
+                >
+                    Register
+                </button>
+                <p className="text-sm">Have an account? <Link href={'/login'} className="text-violet-400 hover:underline">Login</Link></p>
             </form>
         </div>
     )
