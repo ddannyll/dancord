f18ec5275959b0ebd1d456b37c92029f753c897c
ddannyll <daniel.nguyen.s173@gmail.com>
2023-05-24 22:41:40 +1000

    clean imports

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 
 import type { AppProps } from 'next/app';
-export default function MyApp({ Component, pageProps: {session, ...pageProps} }: AppProps) {
+export default function MyApp({ Component, pageProps: {...pageProps} }: AppProps) {
     return (
         <Component {...pageProps} />
     )
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,7 +2,6 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 export default function Index() {
-    const router = useRouter()
     return <div>
         <Link href={'/login'}>login</Link>
         <Link href={'/register'}>register</Link>
