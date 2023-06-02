79583a67fa87b9ea275795fb1e4b322149b65c70
ddannyll <daniel.nguyen.s173@gmail.com>
2023-06-02 18:12:39 +1000

    route user to login if auth details msising:

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,8 +1,20 @@
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Sidebar } from './Sidebar';
+import { useContext, useEffect } from 'react';
+import { AuthContext } from '@/pages/_app';
+import { useRouter } from 'next/router';
 
 export default function Layout({children} : {children?: React.ReactNode}) {
+    const {authUser} = useContext(AuthContext)
+    const router = useRouter()
+    useEffect(() => {
+        if (!authUser?.token) {
+            router.push('/login')
+
+        }
+    }, [authUser?.token, router])
+
     return <div className='bg-zinc-700 w-screen h-screen flex'>
         <Sidebar />
         <main className='grow'>
