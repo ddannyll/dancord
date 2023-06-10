4fd603f37b6af15508e16e8100dd409e2f696356
ddannyll <daniel.nguyen.s173@gmail.com>
2023-06-10 11:53:41 +1000

    mend

--- frontend/src/pages/server/[serverId]/index.tsx
+++ frontend/src/pages/server/[serverId]/index.tsx
@@ -6,11 +6,6 @@ import { useContext } from 'react';
 import useSWR from 'swr'
 
 export default function Server() {
-    const router = useRouter()
-    const {authUser} = useContext(AuthContext)
-    const serverId = router.query.serverId
-    const {data: serverDetails, isLoading, error} = useSWR({token: authUser?.token, serverId}, fetchServer)
-
     return <Layout>
         <div className="text-slate-50">
             Select a channel cuh
